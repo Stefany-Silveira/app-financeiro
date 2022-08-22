66418cf5a7422c15070e3ccea862283581bea995
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-22 20:53:21 -0300

    add @ Módulos e Reflection

--- src/br/com/stefany/app/financeiro/Teste.java
+++ src/br/com/stefany/app/financeiro/Teste.java
@@ -3,6 +3,8 @@ package br.com.stefany.app.financeiro;
 import br.com.stefany.app.calculo.Calculadora;
 import br.com.stefany.app.calculo.interno.OperacoesAritmeticas;
 
+import java.lang.reflect.Field;
+
 public class Teste {
 
     public static void main(String[] args) {
@@ -12,5 +14,16 @@ public class Teste {
 
         OperacoesAritmeticas op = new OperacoesAritmeticas();
         System.out.println(op.soma(4, 5, 6));
+
+        try {
+            Field fieldId = calc.getClass().getDeclaredFields()[0];
+            fieldId.setAccessible(true);
+            fieldId.set(calc, "def");
+            fieldId.setAccessible(false);
+
+            System.out.println(calc.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
